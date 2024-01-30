690b792664291bd58f82e18e5c1e0645357a286a
User <User@192.168.219.112>
2024-01-30 16:12:00 +0900

    고성언 커밋

--- GitTest01/src/Ex01.java
+++ GitTest01/src/Ex01.java
@@ -6,6 +6,14 @@ public class Ex01 {
 		System.out.println("테스트 파일입니다.");
 		System.out.println("팀원1 두번째 커밋!");
 		System.out.println("팀원2 세번째 커밋!");
+		
+		for(int j = 0; j<5;j++) {
+		for(int i = 0; i<j;i++) {
+			
+			System.out.print("*");
+		}
+		System.out.println();
+	}
 	}
 
 }
