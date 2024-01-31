94e91a529778f8fcc82524d3a7bdc2540a285ad8
User <User@192.168.219.109>
2024-01-31 12:05:36 +0900

    김시은 커밋

--- GitTest01/src/model/StockVO.java
+++ GitTest01/src/model/StockVO.java
@@ -2,47 +2,7 @@ package model;
 
 public class StockVO {
 	
-	private String user_id;
-	private String user_pw;
-	private int my_money;
 	
-	
-	public StockVO(String user_id, String user_pw, int my_money) {
-		super();
-		this.user_id = user_id;
-		this.user_pw = user_pw;
-		this.my_money = my_money;
-	}
-
-
-	public String getUser_id() {
-		return user_id;
-	}
-
-
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
-	}
-
-
-	public String getUser_pw() {
-		return user_pw;
-	}
-
-
-	public void setUser_pw(String user_pw) {
-		this.user_pw = user_pw;
-	}
-
-
-	public int getMy_money() {
-		return my_money;
-	}
-
-
-	public void setMy_money(int my_money) {
-		this.my_money = my_money;
-	}
 	
 	
 	
