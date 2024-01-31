5f2b742ce8e8ded0f2a9cd51da1625a25da4b968
jxehxn <wkdwogns2345@naver.com>
2024-01-31 20:22:45 +0900

    -jh

--- GitTest01/src/controller/MainController.java
+++ GitTest01/src/controller/MainController.java
@@ -1,64 +1,18 @@
 package controller;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
-
-import model.StockVO;
 
 public class MainController extends DBcontroller {
 
-	Connection conn;
-	PreparedStatement psmt;
-	ResultSet rs;
-	public static void main(String[] args) {
-		int row;
-
-		int[] now_price = new int[20];
-		ArrayList<StockVO> results = new ArrayList<>(); // 등락률 보내기 위한 최종 어레이리스트
-		ArrayList<StockVO> svoList = new ArrayList<StockVO>(); // 주식정보 어레이리스트
+	public void stock_Rate_Update() {
 		float[] stock_rate = new float[20];
+//		String[] stock_name = new String[20];
 		Random random = new Random();
-		float randomFloat = random.nextFloat();
-
-		getConn();
-
-		// 동적로딩
-		try {
-			// sql통과 통로
-			String sql = "select * from all_stock";
-			psmt = conn.prepareStatement(sql);
-
-			// sql통과
-			rs = psmt.executeQuery();
-
-			// select 한줄의 데이터 확인 rs.next()
-
-			while (rs.next()) {
-				String stockName = rs.getString("stock_name");
-				int nowPrice = rs.getInt("stock_now_price");
-				int yesterdayPrice = rs.getInt("stock_yesterday_Price");
-				int stockrate = rs.getInt("stock_rate");
-
-				StockVO svo = new StockVO(stockName, yesterdayPrice, nowPrice, stockrate);
-				svoList.add(svo);
-
-			}
-			System.out.println(svoList);
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			allClose();
-		}
-	}
-}
-
-//	public void stock_Rate_Update() {
-
+		int row;
+		
 //		for (int i = 0; i < stock_rate.length; i++) {
 //			float randomFloatInRange = random.nextFloat(60) - 30; // 등락률 퍼센트로 -30 ~ 30를 나타냄
 //			stock_rate[i] = randomFloatInRange / 100;
@@ -66,13 +20,56 @@ public class MainController extends DBcontroller {
 //			System.out.println(randomFloatInRange); // 등락률 퍼센트로 -30 ~ 30를 나타냄
 //		}
 
+		getConn();
+		ArrayList<String> stock_name =  new ArrayList<>();
+		// 동적로딩
+		try {
+			// sql통과 통로
+			String sql = "select * from ALL_STOCK";
+			psmt = conn.prepareStatement(sql);
+
+			// sql통과
+			rs = psmt.executeQuery();
+
+			// select 한줄의 데이터 확인 rs.next()
+			
+			while (rs.next()) {
+				String name = rs.getString(1);
+				int now_price = rs.getInt(2);
+				int yester_price = rs.getInt(3);
+				int rate = rs.getInt(4);
+				stock_name.add(name);
+			}
+			System.out.println(stock_name.get(2));
+			stock_name.get(1);
+			stock_name.get(2);
+			stock_name.get(3);
+			
+//					System.out.println(stock_name[2]);
+				
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			allClose();
+		}
+		
+		
+		
+		
+		
+		
+		
+		//=================
 //		getConn();
 //		for (int i = 0; i < 20; i++) {
 //			try {
 //				String sql = "UPDATE ALL_stock SET STOCK_RATE = ? WHERE STOCK_NAME = ?";
 //				float rate = stock_rate[i]; // 등락률 집어 넣기
 //				String name = 0; /// 0 대신 주식 이름 넣어야 함.
+//				
 //				psmt = conn.prepareStatement(sql);
+//				
 //				psmt.setFloat(1, rate);
 //				psmt.setString(2, name);
 //
@@ -105,5 +102,6 @@ public class MainController extends DBcontroller {
 //			}
 //		}
 
-//	}
-//}
+	}
+
+}
---
+++ GitTest01/src/controller/testMain.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class testMain {
+
+	public static void main(String[] args) {
+		MainController mainController = new MainController();
+		// TODO Auto-generated method stub
+		mainController.stock_Rate_Update();
+		
+	}
+
+}
