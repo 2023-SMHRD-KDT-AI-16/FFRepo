1dbc707c2ac074a35eefce4e9723d91cb3d33852
User <User@192.168.219.109>
2024-01-31 19:02:27 +0900

    tldms commit

--- GitTest01/src/view/MainView.java
+++ GitTest01/src/view/MainView.java
@@ -11,323 +11,253 @@ import model.UserVO;
 public class MainView {
 
 	public static void main(String[] args) {
-		int my_money=50000000; 
-		int my_yield  = 0;
+		int my_money = 50000000;
+		int my_yield = 0;
 		String my_item = null;
-		
+
 		DBcontroller mdao = new DBcontroller();
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		int sel; // 사용자의 선택
-		
-		while(true) {
+
+		while (true) {
 			System.out.println("*개미는 뚠뚠*");
 			System.out.println("[1]회원가입 [2]로그인 [3]게임시작 [4]전체순위조회 [5]종료");
 			sel = sc.nextInt();
-			
-			switch(sel) {
+
+			switch (sel) {
 			case 5:
 				System.out.println("종료되었습니다.");
 				break;
-				//종료
-			
-			
+			// 종료
+
 			case 1:
-				//회원가입
+				// 회원가입
 				System.out.println("====회원가입 창입니다====");
 				System.out.println("[1]뒤로가기 [2]회원가입 진행");
 				sel = sc.nextInt();
-				
-				 if(sel ==1) {//뒤로가기
-						break;
+
+				if (sel == 1) {// 뒤로가기
+					break;
+				} else if (sel == 2) {// 회원가입 진행
+
+					System.out.print("id를 입력하세요 : ");
+					String user_id = sc.next();
+
+					// ID 중복시 실행 if(join_id == )
+
+					System.out.print("pw를 입력하세요 : ");
+					String user_pw = sc.next();
+
+					mdao = new DBcontroller();
+
+					UserVO uv = new UserVO(user_id, user_pw, my_money, my_yield);
+					int row = mdao.insertMember(uv);
+
+					if (row > 0) {
+						System.out.println("회원가입 성공!");
+					} else {
+						System.out.println("회원가입 실패!");
 					}
-				else if(sel==2) {//회원가입 진행
-				
-				System.out.print("id를 입력하세요 : ");
-				String user_id = sc.next();
-				
-				//	ID 중복시 실행	 if(join_id ==  )
-				
-				System.out.print("pw를 입력하세요 : ");
-				String user_pw = sc.next();
-				
-				mdao = new DBcontroller();
-				
-				UserVO uv = new UserVO(user_id, user_pw, my_money, my_yield) ;
-				int row = mdao.insertMember(uv);
-				
-				if(row>0) {
-					System.out.println("회원가입 성공!");
-				}else {
-					System.out.println("회원가입 실패!");
-				}
-				
-				
-				
-				
-				break;
-				// DB에 회원가입정보 저장
-				// DAO ?? = new DAO();
-				// DTO ?? = new DTO(user_id,user_pw);
-				
-				
-				
-				}
-				else {//잘못입력
+
+					break;
+					// DB에 회원가입정보 저장
+					// DAO ?? = new DAO();
+					// DTO ?? = new DTO(user_id,user_pw);
+
+				} else {// 잘못입력
 					System.out.println("잘못 입력된 값 입니다.");
 					System.out.println();
 					break;
 				}
-				
-				
-				
+
 			case 2:
-				//로그인
+				// 로그인
 				System.out.println("====로그인 창입니다====");
 				System.out.println("[1]뒤로가기 [2]로그인 진행");
 				sel = sc.nextInt();
-				if(sel == 1) {//뒤로가기
+				if (sel == 1) {// 뒤로가기
 					break;
-				}
-				else if(sel == 2) {//로그인 진행
-				
-				
-				System.out.print("id를 입력하세요 : ");
-				String user_id = sc.next();
-				
-				
-				System.out.print("pw를 입력하세요 : ");
-				String user_pw = sc.next();
-				
-				mdao = new DBcontroller();
-				int row = mdao.userLogin(user_id, user_pw);
-				if(row>0) {
-					System.out.println("로그인 성공");
-				}else {
-					System.out.println("로그인 실패");
-				}
-				
-				
-				//join_id와 join_pw가 db에 저장된 값과 일치하면 로그인성공 출력
-				
-				break;
-				
-				
-				
-				}else {//잘못입력
-					System.out.println("잘못된 입력입니다.");
-					System.out.println();
-					break;
-				}
-				
-			case 3:
-				while(true) {
-				//게임시작
-				System.out.println("====게임 화면 ====");
-				System.out.println("[1]메인화면 [2]전체 주식종목 [3]종목검색 [4]보유 주식 [5]아이템 상점 [6]하루 마감하기");
-				sel = sc.nextInt();
-				
-				
-				if(sel==1) {//메인화면 
-					break;
-				}else if(sel ==2) {//전체 주식 종목
-					System.out.println("전체 주식 종목");
+				} else if (sel == 2) {// 로그인 진행
+
+					System.out.print("id를 입력하세요 : ");
+					String user_id = sc.next();
+
+					System.out.print("pw를 입력하세요 : ");
+					String user_pw = sc.next();
+
 					mdao = new DBcontroller();
-					
-					
-					ArrayList<StockVO> svoList = mdao.selectMember();
-					int Stock_cnt = 0; // 주식종목 인덱스
-					
-					
-					for(StockVO e : svoList ) {
-						Stock_cnt++;
-						System.out.println("==="+(Stock_cnt)+"===");
-						System.out.println(e.getStockName());
-						System.out.println("시가 :" + e.getNowPrice());
-						System.out.println("전일가 : " + e.getYesterdayPrice());
-						System.out.println("등락률 : "+e.getRate());
+					int row = mdao.userLogin(user_id, user_pw);
+					if (row > 0) {
+						System.out.println("로그인 성공");
+					} else {
+						System.out.println("로그인 실패");
 					}
-					
-					
-					
-					
-					System.out.println();
-					//해당 인덱스번호 주식으로 이동
-					System.out.println("[1]뒤로가기 [2]번호 입력");
-					sel = sc.nextInt();
-					if(sel == 1) {
+
+					// join_id와 join_pw가 db에 저장된 값과 일치하면 로그인성공 출력
+
 					break;
-					}else if(sel ==2) {
-					
-					int Stock_index = sc.nextInt()-1;
-					
-					break;
-					}else {
-						System.out.println("잘못된 입력입니다.");
-						System.out.println();
-						break;
-					}
-					
-					
-					
-					
-					
-				}else if(sel ==3) {//종목 검색
-					System.out.println("종목검색");
-					
-					System.out.println("[1]뒤로가기 [2]종목 검색하기");
-					sel = sc.nextInt();
-					if(sel == 1) {//뒤로 가기
-						break;
-					}else if(sel == 2) {//종목 검색하기
-						System.out.print("검색할 종목 이름 : ");
-						String stockName = sc.next();
-						
-						//검색한 종목이 DB와 일치하면 해당 종목으로 이동
-						DbStockSearch_controller dao2 = new DbStockSearch_controller();
-						ArrayList<StockVO> SearchList = dao2.searchStock(stockName);
-						
-						
-						
-						
-						//해당 종목 정보 보여주기(출력) -- 현재 시가, 등락율 표시
-						for(StockVO e : SearchList) {
-							System.out.println(e.getStockName());
-							System.out.println(e.getNowPrice());
-						}
-						
-						
-						
-						
-						System.out.println("[1]뒤로가기 [2]매수하기 [3]매도하기");
-						sel =sc.nextInt();
-						if(sel == 1) {//뒤로가기
-							break;
-						}else if(sel == 2) {//매수하기
-							System.out.println("==매수하기 화면==");
-							System.out.println("[1]뒤로가기 [2]매수 수량 입력");
-							int choice = sc.nextInt();
-							if(choice == 1) {//뒤로가기
-								break;
-							}else if(choice == 2) {//매수 수량 입력
-								System.out.print("매수 갯수 : ");
-								int stockCount = sc.nextInt();
-								
-								//매수 메소드 적용 칸
-								System.out.println("주식이름 "+stockCount+"주 매수완료");
-								break;
-							}else {
-//								System.out.println("잘못된 입력입니다.");
-								break;
-							}
-							
-						}else if(sel == 3) {//매도하기
-							System.out.println("==매도하기 화면==");
-							System.out.println("[1]뒤로가기 [2]매도 수량 입력");
-							int choice = sc.nextInt();
-							if(choice == 1) {//뒤로가기
-								break;
-							}else if(choice == 2) {//
-								System.out.print("매도 갯수 : ");
-								int stockCount = sc.nextInt();
-								
-								System.out.println(stockName + stockCount + "주 매도완료");
-								
-							
-								mdao.stockSale(stockName, stockCount);
-								
-								
-							
-							
-							
-							
-							
-							
-								break;
-							
-							
-							}else {
-//								System.out.println("잘못된 입력입니다.");
-							}
-							
-						}
-						
-						
-						
-						
-					}
-					
-					
-				}else if(sel == 4) {//보유 주식
-					System.out.println("보유주식 페이지");
-					System.out.println("[1]뒤로가기 [2]보유주식 이동");
-					int choice = sc.nextInt();
-					if(choice ==1 ) {
-						break;
-					}else if(choice == 2) {
-						System.out.println("보유주식 밑에 나열해야함");
-						//보유주식 나열해야함
-						//보유주식에서 인덱스를골라서 해당 주식페이지로 이동
-						
-						
-					}
-					
-					
-				}else if(sel == 5) {//아이템 상점
-					
-					
-					
-				}else if(sel == 6) {
-					
-				}else {
+
+				} else {// 잘못입력
 					System.out.println("잘못된 입력입니다.");
+					System.out.println();
 					break;
 				}
-				
-				
-				
-				
-				
-				
-				}//while
-				
+
+			case 3:
+
+				while (true) {
+					// 게임시작
+					System.out.println("====게임 화면 ====");
+					System.out.println("[1]메인화면 [2]전체 주식종목 [3]종목검색 [4]보유 주식 [5]아이템 상점 [6]하루 마감하기");
+					sel = sc.nextInt();
+					while (true) {
+
+						if (sel == 1) {// 메인화면
+							break;
+						} else if (sel == 2) {// 전체 주식 종목
+
+							System.out.println("전체 주식 종목");
+							mdao = new DBcontroller();
+
+							ArrayList<StockVO> svoList = mdao.everyStock();
+							int Stock_cnt = 0; // 주식종목 인덱스
+
+							for (StockVO e : svoList) {
+								Stock_cnt++;
+								System.out.println("===" + (Stock_cnt) + "===");
+								System.out.println(e.getStockName());
+								System.out.println("시가 :" + e.getNowPrice());
+								System.out.println("전일가 : " + e.getYesterdayPrice());
+								System.out.println("등락률 : " + e.getRate());
+							}
+
+							System.out.println();
+
+							// 해당 인덱스번호 주식으로 이동
+
+							System.out.println("[1]뒤로가기 [2]번호 입력");
+							sel = sc.nextInt();
+							if (sel == 1) {
+								break;
+							} else if (sel == 2) {
+
+								int Stock_index = sc.nextInt() - 1;
+
+								break;
+							} else {
+								System.out.println("잘못된 입력입니다1.");
+								System.out.println();
+								break;
+							}
+
+						} else if (sel == 3) {// 종목 검색
+							System.out.println("종목검색");
+
+							System.out.println("[1]뒤로가기 [2]종목 검색하기");
+							sel = sc.nextInt();
+							if (sel == 1) {// 뒤로 가기
+								break;
+							} else if (sel == 2) {// 종목 검색하기
+								System.out.print("검색할 종목 이름 : ");
+								String stockName = sc.next();
+
+								// 검색한 종목이 DB와 일치하면 해당 종목으로 이동
+								DbStockSearch_controller dao2 = new DbStockSearch_controller();
+								ArrayList<StockVO> SearchList = dao2.searchStock(stockName);
+
+								// 해당 종목 정보 보여주기(출력) -- 현재 시가, 등락율 표시
+								for (StockVO e : SearchList) {
+									System.out.println(e.getStockName());
+									System.out.println(e.getNowPrice());
+								}
+
+								System.out.println("[1]뒤로가기 [2]매수하기 [3]매도하기");
+								sel = sc.nextInt();
+								if (sel == 1) {// 뒤로가기
+									break;
+								} else if (sel == 2) {// 매수하기
+									System.out.println("==매수하기 화면==");
+									System.out.println("[1]뒤로가기 [2]매수 수량 입력");
+									int choice = sc.nextInt();
+									if (choice == 1) {// 뒤로가기
+										break;
+									} else if (choice == 2) {// 매수 수량 입력
+										System.out.print("매수 갯수 : ");
+										int stockCount = sc.nextInt();
+
+										// 매수 메소드 적용 칸
+										System.out.println("주식이름 " + stockCount + "주 매수완료");
+										break;
+									} else {
+//								System.out.println("잘못된 입력입니다.");
+										break;
+									}
+
+								} else if (sel == 3) {// 매도하기
+									System.out.println("==매도하기 화면==");
+									System.out.println("[1]뒤로가기 [2]매도 수량 입력");
+									int choice = sc.nextInt();
+									if (choice == 1) {// 뒤로가기
+										break;
+									} else if (choice == 2) {//
+										System.out.print("매도 갯수 : ");
+										int stockCount = sc.nextInt();
+
+										System.out.println(stockName + stockCount + "주 매도완료");
+
+										mdao.stockSale(stockName, stockCount);
+
+										break;
+
+									} else {
+//								System.out.println("잘못된 입력입니다.");
+									}
+
+								}
+
+							}
+
+						} else if (sel == 4) {// 보유 주식
+							System.out.println("보유주식 페이지");
+							System.out.println("[1]뒤로가기 [2]보유주식 이동");
+							int choice = sc.nextInt();
+							if (choice == 1) {
+								break;
+							} else if (choice == 2) {
+								System.out.println("보유주식 밑에 나열해야함");
+								// 보유주식 나열해야함
+								// 보유주식에서 인덱스를골라서 해당 주식페이지로 이동
+
+							}
+
+						} else if (sel == 5) {// 아이템 상점
+
+						} else if (sel == 6) {
+
+						} else {
+							System.out.println("잘못된 입력입니다.2");
+							break;
+						}
+					}
+
+					break;
+
+				} // while
+				break;
+
 			case 4:
-				//전체순위조회
-			default :
-				System.out.println("잘못된 입력입니다.");
+				// 전체순위조회
+
+			default:
+				System.out.println("잘못된 입력입니다.3");
 				System.out.println();
-				
-			
-			
-			
-			}//switch sel
-			
-			
-			
-			
-			
+
+			}// switch sel
+
 		}
-		
-		
-		
-	        
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 //  =====================아래것은 MP3 프로젝트 할때 클래스들이니 참고하세요 주석처리함.
 //		//View 의 역할 : 사용자의 인터페이스 U
 //		//기능을 사용자가 선택할 수 있도록 만들기.
@@ -399,7 +329,7 @@ public class MainView {
 //        f.setVisible(true);
 
 //	new Button1("버튼 만들기");
-	
+
 //	  JFrame frame = new JFrame("Input Box 예제");
 //        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //        frame.setSize(300, 150);
