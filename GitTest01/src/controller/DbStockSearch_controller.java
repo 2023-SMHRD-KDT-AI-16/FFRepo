1a644b7a938d36b8775e9e2f9f7928dbf47c4b66
User <User@192.168.219.112>
2024-01-31 16:21:42 +0900

    고성언 ㅇㅇ

--- GitTest01/src/controller/DbStockSearch_controller.java
+++ GitTest01/src/controller/DbStockSearch_controller.java
@@ -17,21 +17,21 @@ public class DbStockSearch_controller extends DBcontroller {
 			// sql통과 통로
 			String sql = "select *  from ALL_STOCK where STOCK_NAME = ?";
 
-			// ?채우기 - ?가 없으면 생략
-			psmt.setString(1, search_stock_name);
 			// sql통과 하세요!
 			psmt = conn.prepareStatement(sql);
 			//
+			// ?채우기 - ?가 없으면 생략
+			psmt.setString(1, search_stock_name);
 
 			rs = psmt.executeQuery();
 
 			while (rs.next()) {
-				String stockName = rs.getString(1);
-				int stockCount = rs.getInt(2);
-				int buyPrice = rs.getInt(3);
-				int nowPrice = rs.getInt(4);
+				String stockName = rs.getString("STOCK_NAME");
+				int stockCount = rs.getInt("STOCK_YESTERDAY_PRICE");
+				int nowPrice = rs.getInt("STOCK_NOW_PRICE");
+				int anything = rs.getInt("STOCK_RATE");
 
-				StockVO sdvo = new StockVO(stockName, stockCount, buyPrice, nowPrice);
+				StockVO sdvo = new StockVO(stockName, stockCount, nowPrice, anything);
 				usertoList.add(sdvo);
 			}
 			return usertoList;
