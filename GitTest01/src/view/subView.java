317f252f657e95690a288140c6a73c3e1fb74a27
jxehxn <wkdwogns2345@naver.com>
2024-02-01 19:04:21 +0900

    Merge branch 'master' of https://github.com/2023-SMHRD-KDT-AI-16/FFRepo.git

--- GitTest01/src/view/subView.java
+++ GitTest01/src/view/subView.java
@@ -14,68 +14,50 @@ public class subView {
 	protected PreparedStatement psmt;
 	protected ResultSet rs;
 
-	public void gameEnd() {
-		
+	public void gameEnd(String name, int score) {
+
 		System.out.println("[1]점수 등록 [2]종료");
 		Scanner sc = new Scanner(System.in);
 		int cho = sc.nextInt();
-		
+
 		switch (cho) {
 		case 1: {
-
 
 			dbcon.getConn();
 
 			try {
-				String sql_1 = "select NAME, SCORE, RANK_SCORE from RANK";
-				rs = psmt.executeQuery();
-				
+				String sql = "insert into my_user values(?,?,?)";
+				psmt = conn.prepareStatement(sql);
+
 				String NAME = null;
 				int SCORE = 0;
 				int RANK_SCORE = 0;
-				
-				while (rs.next()) {
-					float id = rs.getFloat("stock_yield");
-					int my_price = rs.getInt("purchased_stock_amount");
-					final_yield += id;
-					cnt +=1 ;
-					sum += my_price;
-
-				}
-				
-				// sql통과 통로
-				String sql_2 = "insert into my_user values(?,?,?)";
-				psmt = conn.prepareStatement(sql_2);
 
 				// ?채우기
-				psmt.setString(1,Nickname);
-				psmt.setInt(2, sum);
-				psmt.setFloat(3, final_yield);
+				psmt.setString(1, NAME);
+				psmt.setInt(2, SCORE);
+				psmt.setFloat(3, RANK_SCORE);
 
 				// sql통과 하세요!
-				int row = psmt.executeUpdate();
-				return row;
-
+				psmt.executeUpdate();
 			} catch (SQLException e) {
 				e.printStackTrace();
-				return 0;
 			} finally {
-				allClose();
+				dbcon.allClose();
 			}
-			
-			
+
 			break;
 		}
 		case 2: {
-			
+
 			break;
 		}
 		default:
 			break;
 		}
 	}
-	
+
 	public void rank() {
-		
+
 	}
 }
