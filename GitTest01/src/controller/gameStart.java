da449042612bd33e3d10a093009579120858432c
jxehxn <wkdwogns2345@naver.com>
2024-02-01 09:58:11 +0900

    -jh

--- GitTest01/src/controller/gameStart.java
+++ GitTest01/src/controller/gameStart.java
@@ -15,9 +15,10 @@ public class gameStart {
 	void start() {
 		// TODO Auto-generated method stub
 		for (int i = 0; i < 20; i++) {
-			System.out.print(dbco.everyStock().get(i).getStockName() + "\t \t" +dbco.everyStock().get(i).getYesterdayPrice()
+			System.out.print(dbco.everyStock().get(i).getStockName() + "\t \t \t" +dbco.everyStock().get(i).getYesterdayPrice()
 					+ "\t \t" + dbco.everyStock().get(i).getNowPrice() + "\t \t"+ dbco.everyStock().get(i).getRate());
 		}
+		
 
 //	switch (key) {
 //	case value: {
