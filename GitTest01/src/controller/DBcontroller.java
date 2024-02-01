72e6fa2aa990c81890b2713bd6e400ec7aa4cf29
User <User@192.168.219.112>
2024-02-01 12:13:03 +0900

    Merge branch 'master' of https://github.com/2023-SMHRD-KDT-AI-16/FFRepo

--- GitTest01/src/controller/DBcontroller.java
+++ GitTest01/src/controller/DBcontroller.java
@@ -56,29 +56,8 @@ public class DBcontroller {
 
 	}
 
-	// 주식 판매 기능 메소드
-	public int stockSale(String sale_stock_name) {
-
-		getConn();
-
-		try {
-			// sql통과 통로
-			String sql = "delete from my_stock where stock_name = ?";
-			psmt = conn.prepareStatement(sql);
-
-			// ?채우기 - ?가 없으면 생략
-			psmt.setString(1, sale_stock_name);
-			// sql통과 하세요!
-			int row = psmt.executeUpdate();
-			return row;
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return 0;
-		} finally {
-			allClose();
-		}
-	}
+	
+	
 
 	// 1. 회원가입 기능
 
@@ -218,7 +197,7 @@ public class DBcontroller {
 			}
 			
 			
-			String sql_2 = "select stock_count, purchase_stock_amount from my_stock where stock_name = ?";
+			String sql_2 = "select stock_count, purchased_stock_amount from my_stock where stock_name = ?";
 			psmt = conn.prepareStatement(sql_2);
 
 			// ? 채우기
